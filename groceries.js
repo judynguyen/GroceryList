4d5e9eb8371c80b4b3cae12e3013340f77474948
Jude <jnguyen41@mail.csuchico.edu>
2016-11-30 16:53:56 -0800

    if statement

--- groceries.js
+++ groceries.js
@@ -29,10 +29,12 @@ function loadCookieList(){
   var cookieList = getCookie("mls");
   var arrayCookie = cookieList.split(",");
   console.log(arrayCookie);
+  if(arrayCookie.length>0){
   for(var i=0; i<arrayCookie.length;i++){
     var arrayIndex = arrayCookie[i];
     console.log("index at "+ i + " = " + arrayIndex);
     displayItem(arrayIndex);
+    }
   }
 }
 
